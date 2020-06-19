977f15d7c0b4f236dd658652033638aa2bb2e086
FordRelak <FordRelak.4050@yandex.ru>
2020-06-19 15:43:35 +0300

    Добавьте файлы проекта.

---
+++ BSalonWebApp/Pages/Record.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BSalonWebApp.Pages
+{
+    public class RecordModel : PageModel
+    {
+        public void OnGet()
+        {
+
+
+        }
+    }
+}
