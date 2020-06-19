be3773387fb1ade6e144b0af5b3e55b31f62f1d2
FordRelak <FordRelak.4050@yandex.ru>
2020-06-19 19:56:13 +0300

    AddShowMoreBtn

---
+++ BSalonWebApp/wwwroot/js/ShowMore.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function ($) {
+    $("#showMore").click(function (e) {
+        $(".imagelist img:hidden").slice(0, 3).fadeIn();
+        if ($(".imagelist img:hidden").length < 1) $(this).fadeOut();
+    })
+})
