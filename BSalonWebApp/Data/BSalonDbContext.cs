057f4b4b272943e8c5eab934abd627172ed07f1b
ForeverFast <59344813+ForeverFast@users.noreply.github.com>
2020-06-19 19:55:43 +0300

    Создал структуру

---
+++ BSalonWebApp/Data/BSalonDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BSalonWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BSalonWebApp.Data
+{
+    public class BSalonDbContext : DbContext
+    {
+        public DbSet<WorkDay> WorkDays { get; set; }
+
+        public BSalonDbContext(DbContextOptions<BSalonDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ BSalonWebApp/Models/WorkDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSalonWebApp.Models
+{
+    public class WorkDay
+    {
+        public IEnumerable<Record> Records { get; set; }
+
+    }
+}
