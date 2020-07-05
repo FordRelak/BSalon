dd2311cebb2465525063040dc23c2309bb4cc22c
FordRelak <FordRelak.4050@yandex.ru>
2020-07-05 16:32:09 +0300

    Картинки выбираются по сезону года

--- BSalonWebApp/Pages/Index.cshtml.cs
+++ BSalonWebApp/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using BSalonWebApp.Data;
 using BSalonWebApp.Models.SalonServices;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -16,7 +17,37 @@ namespace BSalonWebApp.Pages
         public IndexModel(BSalonDbContext context) =>
             _context = context;
 
-        public async Task OnGetAsync() =>
+        public async Task OnGetAsync()
+        {
             Services = await _context.Services.ToListAsync();
+
+            switch (DateTime.Now.Month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    ViewData["Season"] = "Winter";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    ViewData["Season"] = "Spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    ViewData["Season"] = "Summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    ViewData["Season"] = "Autumm";
+                    break;
+                default:
+                    break;
+            }
+
+            ViewData["AmountImgs"] = (int)(new System.IO.DirectoryInfo(System.IO.Path.Combine(Environment.CurrentDirectory, "wwwroot", "imgs", (string)ViewData["Season"])).GetFiles().Length);
+        }
     }
 }
